50db142bc1a3a5d4b4ec565b2004c6c301c7279d
WildhanValerian25 <wildanvalerian89@gmail.com>
2025-05-05 16:08:35 +0700

    Perbaiki deklarasi class siswa

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -13,4 +13,12 @@ public:
     friend class siswa; // Syntax diperbaiki: titik dua (:) jadi titik koma (;)
 };
 
+class siswa {
+private:
+    int id;
+
+public:
+    void setId(int pId);
+    void displayAll(orang &a); // Parameter diubah dari pointer ke reference
+};
 
