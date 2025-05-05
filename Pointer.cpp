b71968464b0e9e4ae96aa6c938a52e4396c7c09f
WildhanValerian25 <wildanvalerian89@gmail.com>
2025-05-05 15:16:04 +0700

    Menambhkan class staf

--- Pointer.cpp
+++ Pointer.cpp
@@ -8,6 +8,16 @@ class Dosen{
         cout << "Namanya adalah " << nama << endl
     }
 };
+class Staf{
+    public:
+    int nidn;
+};
+int main (){
+    Dosen ds;
+    ds.nama = "Giga";
+    ds.tampilNama();
+
+    
 
 
     
